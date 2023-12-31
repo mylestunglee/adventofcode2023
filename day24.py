1f2071e0c5299e14ce2ace652b6ed65cee158981
Myles Lee <mylestunglee@gmail.com>
2023-12-31 01:24:55 +0000

    solve all problems

--- day24.py
+++ day24.py
@@ -1,6 +1,8 @@
 import sys
 from collections import namedtuple
 import numpy as np
+import scipy
+import random
 
 Projectile = namedtuple('Projectile', ['position', 'velocity'])
 
@@ -38,8 +40,44 @@ def intersect(proj1, proj2):
 def solve_part1(projectiles):
     return sum(1 for i1, proj1 in enumerate(projectiles) for i2, proj2 in enumerate(projectiles) if i1 < i2 and intersect(proj1, proj2))
 
+def minimum_distance(proj1, proj2):
+    a = proj2.position - proj1.position
+    b = proj2.velocity - proj1.velocity
+    if b.dot(b) == 0:
+        return np.NAN
+    mu = -(a.dot(b)) / b.dot(b)
+    c = a + mu * b
+    return c.dot(c)
+
+def minimum_distances(proj1, projectiles):
+    return sum(minimum_distance(proj1, proj2) for proj2 in projectiles)
+
+def solve_velocity(projectiles, y0, w0):
+    def cost(x):
+        proj1 = Projectile(x[:3], x[3:])
+        return minimum_distances(proj1, projectiles)
+
+    x0 = np.concatenate((y0, w0))
+    res = scipy.optimize.minimize(cost, x0, method='Nelder-Mead', tol=1e-12)
+    print(res)
+    return res.x[3:]
+
+def solve_position(projectiles, y0, w):
+    def cost(x):
+        proj1 = Projectile(x, w)
+        return minimum_distances(proj1, projectiles)
+
+    res = scipy.optimize.minimize(cost, y0, method='Nelder-Mead', tol=1e-12)
+    print(res)
+    return res.x
+
 def solve_part2(projectiles):
-    pass
+    proj = random.choice(projectiles)
+    w = solve_velocity(projectiles, proj.position, proj.velocity)
+    # assert w's are integers
+    y = solve_position(projectiles, proj.position, w)
+    # assert y's are integers
+    return int(sum(y))
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as file:
@@ -47,4 +85,4 @@ if __name__ == '__main__':
 
     projectiles = parse_puzzle_input(puzzle_input)
     print('part1', solve_part1(projectiles))
-    #print('part2', solve_part2(projectiles))
+    print('part2', solve_part2(projectiles))
